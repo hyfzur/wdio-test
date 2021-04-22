7405916680e54f01dda0e0a59e16e0e812737291
Md Hifzur Rahman <md.rahman3@wipro.com>
2021-04-22 19:56:32 +0530

    test5

--- wdio.conf.js
+++ wdio.conf.js
@@ -60,7 +60,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 2,
         //
-        browserName: 'firefox',
+        browserName: 'chrome',
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
@@ -114,12 +114,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone', [TimelineService], 'screenshots-cleanup'],
-    // clean screenshots
-    cleanScreenshotsFolder: {
-        folder: './timeline-report',
-        pattern: '*.jpeg'
-    },
+    services: ['chromedriver', [TimelineService]],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
